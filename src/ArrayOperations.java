12800c2596d5be5dcfa463a93a7b6d0234572ecc
30xintegral <elvinmahmudzade04@gmail.com>
2024-07-21 00:22:08 +0400

    some sorting methods added

--- src/ArrayOperations.java
+++ src/ArrayOperations.java
@@ -1,2 +1,57 @@
+import java.util.Arrays;
+
 public class ArrayOperations {
+    public static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i]>arr[j]){
+                    //here we use a binary way to swap elements
+                    arr[i]=arr[i] ^ arr[j];
+                    arr[j]=arr[i] ^ arr[j];
+                    arr[i]=arr[i] ^ arr[j];
+                }
+            }
+        }
+    }
+
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+
+            // Recursively sort elements before and after partition
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    // Method to partition the array
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = (low - 1); // Index of smaller element
+
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than or equal to pivot
+            if (array[j] <= pivot) {
+                i++;
+                // Swap array[i] and array[j]
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        // Swap array[i+1] and array[high] (or pivot)
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1,62,0,-1};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
 }
